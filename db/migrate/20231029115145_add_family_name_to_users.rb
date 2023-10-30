c2973d2784bc0da50dec2c6c9c8656544a466459
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2023-10-30 20:08:32 +0900

    familyモデルの追加

--- db/migrate/20231029115145_add_family_name_to_users.rb
+++ db/migrate/20231029115145_add_family_name_to_users.rb
@@ -1,5 +1,5 @@
 class AddFamilyNameToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :family_name, :string
+    add_reference :users, :family, null: false, foreign_key: true
   end
 end
