a7e3e00f44ee2767ad4fddca1a1b71bb9885e799
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2023-11-04 18:53:42 +0900

    userモデル、familyモデルの追加記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   has_many :incomes
-
+  belongs_to :family, optional: true
 end
